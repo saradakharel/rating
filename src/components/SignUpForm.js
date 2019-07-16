1c680cf98699915335d4a5cf90b4220666bbaccc
aditya745 <thakuraditya72@yahoo.in>
2019-07-16 12:20:01 +0300

    signup form fixed

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,94 +1,145 @@
 import React, { Component } from "react";
-
-class SignUpForm extends Component {
+class RegisterForm extends Component {
   state = {
-    email: "",
-    password: "",
-    name: "",
-    hasAgreed: false
+    fields: {},
+    errors: {}
   };
 
   handleChange = e => {
-    let target = e.target;
-    let value = target.type === "checkbox" ? target.checked : target.value;
-    let name = target.name;
-
+    let fields = this.state.fields;
+    fields[e.target.name] = e.target.value;
     this.setState({
-      [name]: value
+      fields
     });
   };
 
-  handleSubmit = e => {
+  submituserSignUpForm = e => {
     e.preventDefault();
-    console.log("The form was submitted with the following data:");
+    if (this.validateForm()) {
+      let fields = {};
+      fields["username"] = "";
+      fields["emailid"] = "";
+      fields["mobileno"] = "";
+      fields["password"] = "";
+      this.setState({ fields: fields });
+      alert("Form submitted");
+    }
   };
+
+  validateForm() {
+    let fields = this.state.fields;
+    let errors = {};
+    let formIsValid = true;
+
+    if (!fields["username"]) {
+      formIsValid = false;
+      errors["username"] = "*Please enter your username.";
+    }
+
+    if (typeof fields["username"] !== "undefined") {
+      if (!fields["username"].match(/^[a-zA-Z ]*$/)) {
+        formIsValid = false;
+        errors["username"] = "*Please enter alphabet characters only.";
+      }
+    }
+
+    if (!fields["emailid"]) {
+      formIsValid = false;
+      errors["emailid"] = "*Please enter your email-ID.";
+    }
+
+    if (typeof fields["emailid"] !== "undefined") {
+      var pattern = new RegExp(
+        /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
+      );
+      if (!pattern.test(fields["emailid"])) {
+        formIsValid = false;
+        errors["emailid"] = "*Please enter valid email-ID.";
+      }
+    }
+
+    if (!fields["mobileno"]) {
+      formIsValid = false;
+      errors["mobileno"] = "*Please enter your mobile no.";
+    }
+
+    if (typeof fields["mobileno"] !== "undefined") {
+      if (!fields["mobileno"].match(/^[0-9]{10}$/)) {
+        formIsValid = false;
+        errors["mobileno"] = "*Please enter valid mobile no.";
+      }
+    }
+
+    if (!fields["password"]) {
+      formIsValid = false;
+      errors["password"] = "*Please enter your password.";
+    }
+
+    if (typeof fields["password"] !== "undefined") {
+      if (
+        !fields["password"].match(
+          /^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&]).*$/
+        )
+      ) {
+        formIsValid = false;
+        errors["password"] = "*Please enter secure and strong password.";
+      }
+    }
+
+    this.setState({
+      errors: errors
+    });
+    return formIsValid;
+  }
 
   render() {
     return (
-      <div>
-        <form onSubmit={this.handleSubmit} className="form-container">
-          <div className="formField">
-            <label className="formField-label" htmlFor="name">
-              Full Name
-            </label>
+      <div className="form-container">
+        <div className="register">
+          <h3>Sign Up</h3>
+          <form
+            method="post"
+            name="userSignUpForm"
+            onSubmit={this.submituserSignUpForm}
+          >
+            <label>Name</label>
             <input
               type="text"
-              id="name"
-              className="formField-input"
-              placeholder="Enter your full name"
-              name="name"
-              value={this.state.name}
+              name="username"
+              value={this.state.fields.username}
               onChange={this.handleChange}
             />
-          </div>
-          <div className="formField">
-            <label className="formField-label" htmlFor="password">
-              Password
-            </label>
+            <div className="errorMsg">{this.state.errors.username}</div>
+            <label>Email ID:</label>
+            <input
+              type="text"
+              name="emailid"
+              value={this.state.fields.emailid}
+              onChange={this.handleChange}
+            />
+            <div className="errorMsg">{this.state.errors.emailid}</div>
+            <label>Mobile No:</label>
+            <input
+              type="text"
+              name="mobileno"
+              value={this.state.fields.mobileno}
+              onChange={this.handleChange}
+            />
+            <div className="errorMsg">{this.state.errors.mobileno}</div>
+            <label>Password</label>
             <input
               type="password"
-              id="password"
-              className="formField-input"
-              placeholder="Enter your password"
               name="password"
-              value={this.state.password}
+              value={this.state.fields.password}
               onChange={this.handleChange}
             />
-          </div>
-          <div className="formField">
-            <label className="formField-label" htmlFor="email">
-              E-Mail Address
-            </label>
-            <input
-              type="email"
-              id="email"
-              className="formField-input"
-              placeholder="Enter your email"
-              name="email"
-              value={this.state.email}
-              onChange={this.handleChange}
-            />
-          </div>
-
-          <div className="formField">
-            <label className="formField-checkboxLabel">
-              <input
-                className="formField-checkbox"
-                type="checkbox"
-                name="hasAgreed"
-                value={this.state.hasAgreed}
-                onChange={this.handleChange}
-              />
-              I agree all terms and conditions.
-            </label>
-          </div>
-
-          <div className="formField">
-            <button className="button-primary">Sign Up</button>
-          </div>
-        </form>
+            <div className="errorMsg">{this.state.errors.password}</div>
+            <input type="submit" className="button" value="Register" />
+          </form>
+        </div>
       </div>
     );
   }
 }
-export default SignUpForm;
+
+export default RegisterForm;
