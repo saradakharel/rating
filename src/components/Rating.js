629d39ae344a8da271036af52580284004a8ba76
adityagyawali <gyawali_aditya@hotmail.com>
2019-07-16 18:36:34 +0300

    fix rating

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,49 +1,77 @@
-import React from "react";
+import React from 'react'
+import SignUpForm from './SignUpForm'
 
 class Rating extends React.Component {
   state = {
-    rating: this.props.rating || 2.5,
-    temp_rating: null
-  };
+    rating: 3,
+    temp_rating: null,
+    showForm: false
+  }
+
+  handleForm = () => {
+    console.log('handleform', this.state.showForm)
+    this.setState({
+      showForm: true
+    })
+  }
+
+  handleHideForm = () => {
+    console.log('handleHideForm', this.state.showForm)
+    this.setState({
+      showForm: false
+    })
+  }
   rate = rating => {
     this.setState({
       rating: rating,
       temp_rating: rating
-    });
-  };
+    })
+  }
   star_over = rating => {
     this.setState({
-      rating: this.state.rating,
-      temp_rating: this.state.temp_rating
-    });
-  };
+      rating: rating,
+      temp_rating: rating
+    })
+  }
   star_out = () => {
-    this.setState({ rating: this.state.rating });
-  };
+    this.setState({ rating: this.state.rating })
+  }
   render() {
-    console.log(this.props.rating);
-    var stars = [];
+    console.log('rating', this.state.rating)
+    var stars = []
 
     for (var i = 1; i <= 5; i++) {
-      var klass = "star-rating__star";
+      var klass = 'star-rating__star'
 
       if (this.state.rating >= i && this.state.rating != null) {
-        klass += " is-selected";
+        klass += ' is-selected'
       }
 
       stars.push(
         <label
           className={klass}
-          onClick={this.rate.bind(this, i)}
+          onClick={() => {
+            this.rate.bind(this, i)
+            this.handleForm()
+          }}
           onMouseOver={this.star_over.bind(this, i)}
           onMouseOut={this.star_out}
         >
           ★
         </label>
-      );
+      )
     }
 
-    return <div className="star-rating">{stars}</div>;
+    return (
+      <div className="star-rating">
+        {' '}
+        {this.state.showForm ? (
+          <SignUpForm handleHideForm={this.handleHideForm} />
+        ) : (
+          stars
+        )}
+      </div>
+    )
   }
 }
-export default Rating;
+export default Rating
